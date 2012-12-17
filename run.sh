85ada137ce64f0d5b824f154353916e366675417
Andrew Miller <ak.miller@auckland.ac.nz>
2012-12-17 16:35:09 +1300

    Add an additional blank line

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 export API_PATH=$CMISS_ROOT/build/third_party/src/fieldml-build/
 export LD_LIBRARY_PATH=$API_PATH:$API_PATH/core:$API_PATH/io:$LD_LIBRARY_PATH
 export TOPDIR=$(dirname $0)
+
 java -classpath $TOPDIR/target/classes:$TOPDIR/jars/kd.jar:/usr/share/java/scala-library.jar:$API_PATH/jni/fieldml.jar -Djava.library.path=$API_PATH "$@"
